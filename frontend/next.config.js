1a8983e5838b8bcb71b714640bd8618c8d4eaf81
makarka <mkrkru@mail.ru>
2024-07-19 22:31:36 +0300

    frontend init

---
+++ frontend/next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+
+// const withPWA = require('next-pwa')({
+//     dest: 'public',
+//     disable: process.env.NODE_ENV === 'development'
+// });
+
+// module.exports = withPWA({
+//     experimental: {
+//         scrollRestoration: true
+//     },
+//     reactStrictMode: false,
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'storage.twodev.cc'
+//             }
+//         ]
+//     }
+// })
+
+module.exports = {
+    output: 'standalone',
+    reactStrictMode: false
+}
