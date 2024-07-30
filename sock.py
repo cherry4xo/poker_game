1f0dea3bfe877ecc9966ef7a1f1aa063f7ee0d14
cherry4xo <1jlaketuk1@gmail.com>
2024-07-30 15:11:36 +0300

    lil fix

--- sock.py
+++ sock.py
@@ -1,6 +1,7 @@
 from websocket import create_connection
 
-ws = create_connection("wss://api.cherry4xo.ru/poker_game/game/")
+ws = create_connection("wss://0.0.0.0:8081/poker_game/game/1e837479-7c0c-4b2f-bd27-0641e234906f",
+                       header={"username": "cherry4xo"})
 print(ws.recv())
 print("Hello")
 ws.send("data")
