3becf27943eea8ae399fbeb082c73ce647a5f80c
makarka <mkrkru@mail.ru>
2024-09-04 19:49:18 +0300

    fixes

--- services/frontend/components/Common/Controls.tsx
+++ services/frontend/components/Common/Controls.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Button, HStack } from '@chakra-ui/react';
+import { Button, HStack, Text } from '@chakra-ui/react';
 import { useSelector } from '@/redux/hooks';
 import { IPlayer } from '@/utils/types';
 import { useWs } from '@/app/contexts/SocketContext';
@@ -10,7 +10,9 @@ export function Controls() {
     const { user } = useSelector(state => state.misc);
     const { status, seats, players, current_player, owner } = useSelector(state => state.game);
 
+    const toBet = 10;
     const toRaise = 20;
+    const currentbet = players.find((p: IPlayer) => p.id === user?.uuid)?.currentbet ?? 0;
 
     const buttons = [
         [],
@@ -18,11 +20,11 @@ export function Controls() {
             { label: 'Начать игру', color: 'yellow', payload: { type: 'start' } }
         ],
         [
-            { label: `Бет (выбор)`, color: 'teal', payload: { type: 'bet', value: toRaise }, cond: !!players.find((p: IPlayer) => p.id === user?.uuid)?.currentbet },
+            { label: `Бет (${toBet})`, color: 'teal', payload: { type: 'bet', value: toBet } },
             { label: 'Колл', color: 'teal', payload: { type: 'call' } },
-            { label: `Рейз (выбор)`, color: 'teal', payload: { type: 'raise', value: Math.max(...players.map((p: IPlayer) => p.currentbet)) + toRaise } },
+            { label: `Рейз (${toRaise})`, color: 'teal', payload: { type: 'raise', value: Math.max(...players.map((p: IPlayer) => p.currentbet)) + toRaise } },
             { label: 'Фолд/пасс', color: 'red', payload: { type: 'pass' } },
-            { label: 'Чек', color: 'red', payload: { type: 'check' } }
+            // { label: 'Чек', color: 'red', payload: { type: 'check' } }
         ]
     ];
 
@@ -31,7 +33,10 @@ export function Controls() {
                 ? (seats.filter(s => s).length >= 2 && owner === user?.uuid)
                 : true) &&
             buttons[status]
-                .filter(b => b.cond)
+                .filter(b => {
+                    if (b.payload.type === 'bet') return currentbet <= 0;
+                    else return true;
+                })
                 .map((b: any, i: number) =>
                     <Button key={i} isDisabled={seats[current_player ?? 0] !== user?.uuid} h='100%' p='12px 24px' fontSize='18px' rounded='10px' variant='outline' colorScheme={b.color} onClick={() => ws.current.send(JSON.stringify(b.payload))}>{b.label}</Button>)}
     </HStack>;
