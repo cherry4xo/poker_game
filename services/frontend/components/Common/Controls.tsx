bef0e484910a40e24e85e7456f41e7b5fe58342e
makarka <mkrkru@mail.ru>
2024-09-06 22:23:35 +0300

    buttons fix

--- services/frontend/components/Common/Controls.tsx
+++ services/frontend/components/Common/Controls.tsx
@@ -24,13 +24,15 @@ export function Controls() {
             { label: `Рейз (${toRaise})`, color: 'teal', payload: { type: 'raise', value: Math.max(...players.map((p: IPlayer) => p.currentbet)) + toRaise } },
             { label: 'Фолд/пасс', color: 'red', payload: { type: 'pass' } },
             { label: 'Чек', color: 'red', payload: { type: 'check' } }
-        ]
+        ],
+        [],
+        []
     ];
 
     return <HStack h='70px' spacing='8px'>
         {(status === SessionStatus.LOBBY ? (seats.filter(s => s).length >= 2 && owner === user?.uuid) : true) &&
             buttons[status]
-                .filter(b => allowed_actions.includes(b.payload.type))
+                .filter(b => status === SessionStatus.GAME ? allowed_actions.includes(b.payload.type) : true)
                 .map((b, i: number) =>
                     <Button key={i} isDisabled={status === SessionStatus.GAME ? (seats[current_player ?? 0] !== user?.uuid) : false} h='100%' p='12px 24px' fontSize='18px' rounded='10px' variant='outline' colorScheme={b.color} onClick={() => ws.current.send(JSON.stringify(b.payload))}>{b.label}</Button>)}
     </HStack>;
